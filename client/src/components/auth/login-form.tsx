7f64d804cb529041cee1949693cb5b0138c7ee25
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-05-16 00:26:35 +0300

    created a login form

---
+++ client/src/components/auth/login-form.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import FormInput from '@/components/form-input';
+import CardWrapper from '@/components/auth/card-wrapper';
+
+
+
+const LoginForm = () => {
+    const {handleSubmit, reset, register, formState: {errors}, } =useForm({});
+    const save = () =>{
+      reset();
+    }
+    
+    return (
+        <CardWrapper title='Login'>
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit(save)}>
+                <FormInput name='email' type='email' validations={{required: {value: true, message: 'Email required*'}}} label='Email' errors={errors} register={register} placehoder='Enter email'/>
+                <FormInput name='password' type='password' validations={{required: {value: true, message: 'Password required*'}}} label='Password' errors={errors} register={register} placehoder='Enter password'/>
+                <div className="flex items-center justify-between">
+                    <div className="w-full flex items-center gap-1">
+                        <span>New here? </span>
+                        <Button  variant="link" className="font-normal items-center justify-start p-0" size="sm" asChild><Link href={'/auth/register'}>Register now</Link></Button>
+                    </div>
+                    <Button className='flex gap-1 items-center'>Login</Button>
+                </div>
+            </form>            
+        </CardWrapper>
+    );
+}
+
+export default LoginForm;
