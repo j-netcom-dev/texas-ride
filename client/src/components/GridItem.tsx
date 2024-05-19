7143057bb085a49d362d68b13af185ee857d16ba
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-05-19 06:17:43 +0300

    added overflow-auto to item grid for a responsive data table

--- client/src/components/GridItem.tsx
+++ client/src/components/GridItem.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GridItem = ({ title, children }: {title: string, children: React.ReactNode}) => {
   return (
-    <div className='flex flex-col items-center justify-center p-4 shadow bg-white rounded-xl h-[400px]'>
+    <div className='flex flex-col items-center justify-center py-4 md:px-4 shadow bg-white rounded-xl h-[300px] md:h-[400px] overflow-auto'>
       <h3>{title}</h3>
       {children}
     </div>
