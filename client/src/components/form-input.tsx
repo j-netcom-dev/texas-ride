986320d9968a04cac48fcfc4e28f1e18cca025c2
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-05-22 07:47:56 +0300

    styled the required * indicator

--- client/src/components/form-input.tsx
+++ client/src/components/form-input.tsx
@@ -8,8 +8,8 @@ interface InputPropTypes {
 
 const FormInput = ({name, type ='text', label, placehoder, validations ={}, errors ={}, register}: InputPropTypes) => {
   return (
-    <div className="flex flex-col gap-1">
-        {label &&<label className='block text-black' htmlFor={name}>{label}{validations?.required && '*'}</label>}
+    <div className="flex flex-col gap-1 w-full">
+        {label &&<label className='block text-black' htmlFor={name}>{label}{validations?.required && <sup className='text-red-400'> *</sup>}</label>}
         <input className ={`border ${errors[name]? 'border-red-400': 'border-[#12121225]'} px-4 py-2 rounded-md block`} {...register(name, validations)} type={type} placeholder={placehoder}/>
         {errors[name] &&<small className='block text-red-400 text-sm '>{(errors[name]?.message || errors[name]) || "Invalid input value"}</small>}
     </div>
