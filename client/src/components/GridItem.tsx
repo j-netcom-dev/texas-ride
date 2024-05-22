69a2d6ced6e39b5eb7aadb571d6d757f76d0dd29
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-05-22 07:43:43 +0300

    Styled GridItem title

--- client/src/components/GridItem.tsx
+++ client/src/components/GridItem.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const GridItem = ({ title, children }: {title: string, children: React.ReactNode}) => {
+const GridItem = ({ title, children, title_alignment ='center' }: {title: string, children: React.ReactNode, title_alignment?:string}) => {
   return (
     <div className='flex flex-col items-center justify-center py-4 md:px-4 shadow bg-white rounded-xl h-[300px] md:h-[400px] overflow-auto'>
-      <h3>{title}</h3>
+      <h3 className={`py-3 font-semibold uppercase px-4 text-${title_alignment} w-full`}>{title}</h3>
       {children}
     </div>
   )
