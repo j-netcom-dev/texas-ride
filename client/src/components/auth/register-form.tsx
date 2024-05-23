f3e921d2815aa41d323023ee0d83b3c8c24cc483
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-05-23 13:40:00 +0300

    included user role and success screen to the form steps

--- client/src/components/auth/register-form.tsx
+++ client/src/components/auth/register-form.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from 'react';
 import { UserContactsForm, UserNameForm, UserPasswordForm } from './register';
 import { AnimatePresence } from 'framer-motion';
+import UserRole from './register/user-role';
+import Success from './register/success-screen';
 
 
 const RegisterForm = () => {
@@ -11,9 +13,10 @@ const RegisterForm = () => {
     console.log("Submitted");
     
   }
-  return  <AnimatePresence mode='wait'>
-    {step ==1? <UserContactsForm step={step} setStep={setStep}/>: step ==2? <UserPasswordForm step={step} setStep={setStep}/>: <UserNameForm step={step} setStep={setStep}/>}
-  </AnimatePresence>
+  return  step ==0? <UserRole step={step} setStep={setStep}/>: 
+  (<AnimatePresence mode='wait'>
+    {step ==1?<UserNameForm step={step} setStep={setStep}/>: step ==2? <UserContactsForm step={step} setStep={setStep}/>: step ==3? <UserPasswordForm step={step} setStep={setStep}/>: <Success />}
+  </AnimatePresence>)
 }
 
 export default RegisterForm;
