4adb399927e077ecb04724c5252c31eccf9dc649
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-05-23 13:38:43 +0300

    changed bg color and added a top bar

--- client/src/app/auth/layout.tsx
+++ client/src/app/auth/layout.tsx
@@ -1,9 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
-   <section className="w-full">
-    <div className="h-screen px-4 flex items-center justify-center">
-      {children}
+   <section className="w-full h-screen grid grid-rows-[max-content_auto] bg-[#f8f3f5]">
+    <div className="flex justify-between items-center px-8 py-4 border-b shadow-sm">
+      <Button variant={'ghost'} asChild><Link href={'/'}>&larr; Back Home</Link></Button>
+      <h2 className="text-xl uppercase font-bold">Texas Ride</h2>
     </div>
+      <div className=" px-4 flex items-center justify-center ">
+        {children}
+      </div>
    </section>
   );
 }
