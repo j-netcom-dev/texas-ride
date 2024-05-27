b2481371253810f239b6311caab8b3193be4ddc0
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-05-27 09:10:27 +0300

    moved the form submission functionalities from the form component

--- client/src/components/auth/register-form.tsx
+++ client/src/components/auth/register-form.tsx
@@ -9,10 +9,6 @@ import Success from './register/success-screen';
 
 const RegisterForm = () => {
   const [step, setStep] =useState(0);
-  const handleSubmit =() =>{
-    console.log("Submitted");
-    
-  }
   return  step ==0? <UserRole step={step} setStep={setStep}/>: 
   (<AnimatePresence mode='wait'>
     {step ==1?<UserNameForm step={step} setStep={setStep}/>: step ==2? <UserContactsForm step={step} setStep={setStep}/>: step ==3? <UserPasswordForm step={step} setStep={setStep}/>: <Success />}
