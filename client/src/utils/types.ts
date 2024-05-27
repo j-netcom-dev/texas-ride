f9e9d5c0dcbc1daa77fc033cb8b6026638415faa
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-05-27 09:14:47 +0300

    added role and user details type

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -7,3 +7,17 @@ export type UserNameType ={
     first_name: string,
     last_name: string,
 }
+
+export type UserRoleType ={
+    _id: string,
+    role: string
+}
+
+export type UserDetailsType ={
+    first_name?: string,
+    last_name?: string,
+    password?: string,
+    email?: string,
+    phone?: string,
+    role?: string
+}
