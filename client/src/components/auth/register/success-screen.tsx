f26dd4988bbdf73a9919797d05c168bacf3f2614
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-01 14:18:36 +0300

    destroy local storage on success

--- client/src/components/auth/register/success-screen.tsx
+++ client/src/components/auth/register/success-screen.tsx
@@ -10,17 +10,17 @@ import { destroy_user_details, get_user_details } from '@/utils/storage';
 const Success = () => {
   const [email, setEmail] =useState<string>('');
   useEffect(() =>{
-    const { data } =get_user_details(2) as UserData || undefined; 
+    const { data } =get_user_details(2) as UserData;
     if(!data) return;
     setEmail((data.email || '').toString());
-    destroy_user_details();
+    if(email) destroy_user_details();
 }, []);
 
   return (
     <div className="bg-white px-8 py-16 relative w-[400px] max-w-full flex flex-col gap-8 items-center shadow rounded-xl">
         <div className="w-[75px] h-[75px] bg-green-400 rounded-full flex text-white items-center justify-center"><Check size={48}/></div>
         <h2 className='uppercase font-bold text-xl'>Thank you!</h2>
-        <p className='text-center'>Your account was created successfully. Please check your email <b>{email}</b> for further instructions.</p>
+        <p className='text-center'>Your account was created successfully. Please check your email <b>{email}</b> within 24hrs for further instructions.</p>
         <Button className='block w-full text-center' asChild><Link href={'/auth/login'}>Okay</Link></Button>
     </div>
   )
