3ed6ee4794f63537bedb38305678426d85a06c9c
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-01 14:17:17 +0300

    implemented user signup client

--- client/src/services/user-service.tsx
+++ client/src/services/user-service.tsx
@@ -1,6 +1,6 @@
+import { groq } from "next-sanity";
 import { client } from "../lib/studio";
 import { UserDetailsType } from "@/utils/types";
-import { ClientError } from "next-sanity";
 
 export const get_users = async() =>{
     const query =`*[_type =='user'] {
@@ -11,7 +11,24 @@ export const get_users = async() =>{
     
 }
 
+const checkEmailExists = async (email: string): Promise<boolean> => {
+    const query = groq`*[_type == "user" && email == $email][0]{_id}`;
+    const result = await client.fetch(query, { email });
+    return !!result;
+  };
+
+  const checkPhonelExists = async (phone: string): Promise<boolean> => {
+    const query = groq`*[_type == "user" && phone == $phone][0]{_id}`;
+    const result = await client.fetch(query, { phone });
+    return !!result;
+  };
+
+
 export const create_user =async ({first_name, last_name, email, phone, role, password}: UserDetailsType) =>{
+    const userEmailExists =await checkEmailExists(email || '');
+    const userPhoneExists =await checkPhonelExists(phone || '');
+    if(userEmailExists) throw Error("User with email already exist");
+    if(userPhoneExists) throw Error("User with phone number already exist");
     const user = await client.create({
         _type: 'user',
         first_name,
@@ -24,5 +41,6 @@ export const create_user =async ({first_name, last_name, email, phone, role, pas
             _ref: role,
         },
     });
-    
+
+   return {_id: user._id}
 }
