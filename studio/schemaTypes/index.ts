cc4f73bd54754becb6001b333d34d281fd3ac616
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-01 14:17:50 +0300

    installed the OTP schema

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,8 +1,9 @@
-import { VehicleType } from "./VehicleType";
+import { OTP } from "./otp";
 import { MakeType } from "./makeType";
-import { ModelType } from "./modelType";
 import { RideType } from "./rideType";
 import { RoleType } from "./roleType";
 import { UserType } from "./userType";
+import { ModelType } from "./modelType";
+import { VehicleType } from "./VehicleType";
 
-export const schemaTypes = [RoleType, UserType, ModelType, MakeType, VehicleType, RideType]
+export const schemaTypes = [RoleType, UserType, ModelType, MakeType, VehicleType, RideType, OTP, ]
