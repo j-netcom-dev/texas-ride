4eb074971c47fbd2aa93b731567b76fad5d1f782
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-01 14:15:06 +0300

    added the logo

--- client/src/app/auth/layout.tsx
+++ client/src/app/auth/layout.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
+import Image from "next/image";
 import { Toaster } from "sonner";
+import LOGO from '@/assets/img/logo.png';
 import { Button } from "@/components/ui/button";
 
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
@@ -7,12 +9,12 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
    <section className="w-full h-screen grid grid-rows-[max-content_auto] bg-[#f8f3f5]">
     <div className="flex justify-between items-center px-8 py-4 border-b shadow-sm">
       <Button variant={'ghost'} asChild><Link href={'/'}>&larr; Back Home</Link></Button>
-      <h2 className="text-xl uppercase font-bold">Texas Ride</h2>
+      <Image src={LOGO} alt="logo" className="block w-[100px]"/>
     </div>
       <div className=" px-4 flex items-center justify-center ">
         {children}
         <Toaster richColors />
-      </div>
+      </div> 
    </section>
   );
 }
