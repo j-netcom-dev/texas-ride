556fd886ba2cd73f3799354035a24944683fe84e
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-01 14:10:28 +0300

    added logo import and  an icon

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -2,16 +2,18 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import { Logo } from './Logo'
+
 
 export default defineConfig({
   name: 'default',
-  title: '.',
-
+  title: 'Joyful Rides Studio',
+  // icon: <Logo />,
   projectId: 'y20qg5is',
   dataset: 'production',
 
   plugins: [structureTool(), visionTool()],
-
+  
   schema: {
     types: schemaTypes,
   },
