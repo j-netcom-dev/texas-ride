cf4de47af88fd06a908429a2e4b30b07cdbaa232
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-03 00:03:07 +0300

    made the auth page responsive

--- client/src/app/auth/layout.tsx
+++ client/src/app/auth/layout.tsx
@@ -6,11 +6,11 @@ import { Button } from "@/components/ui/button";
 
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
-   <section className="w-full h-screen grid grid-rows-[max-content_auto] bg-[#f8f3f5]">
-    <div className="flex justify-between items-center px-8 py-4 border-b shadow-sm">
-      <Button variant={'ghost'} asChild><Link href={'/'}>&larr; Back Home</Link></Button>
-      <Image src={LOGO} alt="logo" className="block w-[100px]"/>
-    </div>
+   <section className="w-full h-[100svh] grid grid-rows-[max-content_auto] bg-[#f8f3f5]">
+      <div className="flex justify-between items-center pe-3 md:px-8 py-4 border-b shadow-sm">
+        <Button variant={'ghost'} asChild><Link href={'/'} className="flex items-center">&larr; Back Home</Link></Button>
+        <Image src={LOGO} alt="logo" className="block w-[50px] md:w-[100px]"/>
+      </div>
       <div className=" px-4 flex items-center justify-center ">
         {children}
         <Toaster richColors />
--- client/src/components/auth/card-wrapper.tsx
+++ client/src/components/auth/card-wrapper.tsx
@@ -19,12 +19,14 @@ const variants ={
 
 const CardWrapper = ({title, children}: CardWrapperPropTypes) => {
   return (
-    <motion.div className="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 shadow-md overflow-hidden " variants={variants} initial="hidden" animate="visible" exit="exit">
+    <div className="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 shadow-md overflow-hidden">
+      <motion.div variants={variants} initial="hidden" animate="visible" exit="exit">
         <Card className="w-full">
           <CardHeader className="pb-2"><AuthHeader title={title}/></CardHeader>
           <CardContent>{children}</CardContent>
         </Card>
     </motion.div>
+    </div>
   )
 }
 
