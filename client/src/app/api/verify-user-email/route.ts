6d09a7f6f0f80c25304bc77b55d642d627803ebb
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-02 23:22:58 +0300

    set the backend URI as env variable

--- client/src/app/api/verify-user-email/route.ts
+++ client/src/app/api/verify-user-email/route.ts
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
-import { NextResponse } from 'next/server';
 import { randomUUID } from 'crypto';
+import { NextResponse } from 'next/server';
 import { email_verification_template } from '@/assets/templates/email_verification_template';
 
 
@@ -8,7 +8,7 @@ export async function POST(req: Request){
     const {to, name, _id} =await req.json();
     
     const token =`${randomUUID()}${randomUUID()}${randomUUID()}`.replace(/-/g, '');
-    const link =`http://localhost:3000/auth/verify-email/${token}/${_id}`;
+    const link =`${process.env.NEXT_PUBLIC_SANITY_URI}/auth/verify-email/${token}/${_id}`;
     const email_template =email_verification_template({name, link});
     const mail_transporter =nodemailer.createTransport({
         host: process.env.NEXT_PUBLIC_EMAIL_CLIENT_ID,
