5664f66c71bb391e02a6330914408f94af75a69b
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-08 10:22:36 +0300

    creted the riders rides component

--- client/src/app/(protected)/riders/page.tsx
+++ client/src/app/(protected)/riders/page.tsx
@@ -1,165 +1,57 @@
 'use client';
 
-import { useEffect } from 'react';
 import GMap from '@/components/GMap';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { getGeoPositionName } from '@/services/geo-service';
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
-const rides = [
-    {
-        from: 'Austin',
-        to: 'Houston',
-        time: '2024-06-01T01:00:00',
-        vehicle: 'Ford Fiesta',
-        driver: 'John Doe',
-        contact: '555-1234'
-    },
-    {
-        from: 'Dallas',
-        to: 'San Antonio',
-        time: '2024-06-02T02:00:00',
-        vehicle: 'Toyota Camry',
-        driver: 'Jane Smith',
-        contact: '555-5678'
-    },
-    {
-        from: 'El Paso',
-        to: 'Fort Worth',
-        time: '2024-06-03T03:00:00',
-        vehicle: 'Honda Accord',
-        driver: 'Mike Johnson',
-        contact: '555-8765'
-    },
-    {
-        from: 'Arlington',
-        to: 'Corpus Christi',
-        time: '2024-06-04T04:00:00',
-        vehicle: 'Chevrolet Malibu',
-        driver: 'Emily Davis',
-        contact: '555-4321'
-    },
-    {
-        from: 'Plano',
-        to: 'Laredo',
-        time: '2024-06-05T05:00:00',
-        vehicle: 'Nissan Altima',
-        driver: 'Chris Wilson',
-        contact: '555-8761'
-    },
-    {
-        from: 'Lubbock',
-        to: 'Garland',
-        time: '2024-06-06T06:00:00',
-        vehicle: 'Hyundai Sonata',
-        driver: 'Sarah Brown',
-        contact: '555-6543'
-    },
-    {
-        from: 'Irving',
-        to: 'Amarillo',
-        time: '2024-06-07T07:00:00',
-        vehicle: 'Kia Optima',
-        driver: 'David Martinez',
-        contact: '555-7890'
-    },
-    {
-        from: 'Grand Prairie',
-        to: 'McKinney',
-        time: '2024-06-08T08:00:00',
-        vehicle: 'Volkswagen Jetta',
-        driver: 'Linda Rodriguez',
-        contact: '555-4329'
-    },
-    {
-        from: 'Frisco',
-        to: 'Brownsville',
-        time: '2024-06-09T09:00:00',
-        vehicle: 'Subaru Impreza',
-        driver: 'James Lee',
-        contact: '555-1239'
-    },
-    {
-        from: 'Pasadena',
-        to: 'Killeen',
-        time: '2024-06-10T10:00:00',
-        vehicle: 'Mazda 6',
-        driver: 'Patricia White',
-        contact: '555-8763'
-    },
-    {
-        from: 'McAllen',
-        to: 'Waco',
-        time: '2024-06-11T14:00:00',
-        vehicle: 'Ford Fusion',
-        driver: 'Robert Harris',
-        contact: '555-4328'
-    },
-    {
-        from: 'Mesquite',
-        to: 'Carrollton',
-        time: '2024-06-12T12:00:00',
-        vehicle: 'Chrysler 300',
-        driver: 'Elizabeth Clark',
-        contact: '555-5673'
-    }
-];
+import {useEffect, useState} from 'react';
+import {Input} from '@/components/ui/input';
+import {Button} from '@/components/ui/button';
+import Rides from '@/components/riders/Rides';
+import {getGeoPositionName} from '@/services/geo-service';
 
 
 const Riders = () => {
-    const convertTime = (datetime: string) => {
-        const date = new Date(datetime);
-        const hours = date.getHours();
-        const minutes = date.getMinutes();
-        const formattedTime = `${hours <10? '0'+hours: hours}:${minutes < 10 ? '0' + minutes : minutes}`;
-        return formattedTime;
-    };
+    const [q, setQ] =useState<{from?:string, to?:string, date?:string, time?:string}>({});
+    const [query, setQuery] =useState({});
+    const [title, setTitle] = useState('Pre-Scheduled rides');
+
     useEffect(() =>{
         const geo_pos =JSON.parse(localStorage.getItem('pos')!);
         if(!geo_pos) return;
         const get_name =async () =>{
             const loc =await getGeoPositionName(geo_pos);
-            console.log(loc);
-            
         }
-        get_name();
-        
+        get_name().then(r => {});
+
     }, [])
-  return (
-    <div className='grid lg:grid-rows-[500px_auto] min-h-screen gap-8'>
-      <div className="grid lg:grid-cols-[30%_auto] gap-8">
-            <div className="">
-            <div className="flex flex-col gap-4 bg-white p-4 lg:p-8 rounded-xl shadow-sm">
-                <h3 className='uppercase font-semibold'>Find ride</h3>
-                <div className="flex flex-col gap-8 pe-8 h-max">
-                    <Input placeholder='Pickup point'/>
-                    <Input placeholder='Destination'/>
-                    <Button>Search</Button>
+    return (
+        <div className='grid lg:grid-rows-[500px_auto] min-h-screen gap-8'>
+            <div className="grid lg:grid-cols-[30%_auto] gap-8">
+                <div className="">
+                    <div className="flex flex-col gap-4 bg-white p-4 lg:p-8 rounded-xl shadow-sm">
+                        <h3 className='uppercase font-semibold'>Find ride</h3>
+                        <div className="flex flex-col gap-8 pe-4 h-max">
+                            <Input placeholder='Pickup point' onChange={e => setQ({...q, from: e.target.value})}/>
+                            <Input placeholder='Destination' onChange={e => setQ({...q, to: e.target.value})}/>
+                            <div className="flex gap-4">
+                                <div className='w-full'>
+                                    <label>Date</label>
+                                    <Input type='date' onChange={e => setQ({...q, date: e.target.value})}/>
+                                </div>
+                                <div className='w-full'>
+                                    <label>Time</label>
+                                    <Input type='time' onChange={e => setQ({...q, time: e.target.value})}/>
+                                </div>
+
+                            </div>
+
+                            <Button onClick={() =>{setQuery(q); setTitle(`Rides ${q?.to? 'to '+q?.to: q?.from? 'from '+q?.from: 'found'}`)}}>Search</Button>
+                        </div>
+                    </div>
                 </div>
+                <div className="bg-white p-4 lg:p-8 rounded-xl shadow-sm"><GMap /></div>
             </div>
+            <Rides title={title} query={query}/>
         </div>
-        <div className="bg-white p-4 lg:p-8 rounded-xl shadow-sm"><GMap /></div>
-      </div>
-      <div className="flex w-full flex-col gap-4 md:p-4 lg:p-8 rounded-xl shadow-sm md:bg-white">
-        <h2 className='uppercase font-bold text-lg md:text-2xl text-[#333]'>Pre-Scheduled rides</h2>
-        <ul className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-8'>
-            {rides.map(({driver, from, to, time}, index) =><li key={index} className='bg-[#ffffff8f] border p-4 rounded-xl flex flex-col gap-3'>
-                <div className="flex gap-3 items-center">
-                    <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>{`${driver.charAt(0)}${driver.split(' ').at(-1)?.charAt(0)}`}</AvatarFallback>
-                    </Avatar>
-                    <div className="uppercase font-semibold">{driver}</div>
-                </div>
-                <div className="">{from} - {to}</div>
-                <div className="text-muted-foreground text-sm">At {convertTime(time)}</div>
-                <Button>Book</Button>
-            </li>)}
-        </ul>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Riders;
---
+++ client/src/components/riders/Rides.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import Image from "next/image";
+import {useEffect, useState} from "react";
+import {Button} from "@/components/ui/button";
+import NO_RIDES from '@/assets/img/no_rides.png';
+import {fetch_available_rides, search_rides} from "@/services/rides-service";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import Link from "next/link";
+
+
+interface RIDE_TYPE {_id?: string, from?:string, to?:string, time?:string, driver?: Record<string, any>}
+
+const Rides =({title, query}: {title: string, query: {from?:string, to?:string, date?:string, time?:string}}) => {
+    const [rides, setRides] =useState<RIDE_TYPE[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            const available_rides:RIDE_TYPE[] =await fetch_available_rides() as RIDE_TYPE[];
+            setRides(available_rides);
+        })();
+    }, []);
+
+    useEffect(() => {
+        (async () =>{
+            if(Object.keys(query).length){
+                console.log(await search_rides(query));
+            }
+        })();
+    }, [title, query]);
+
+    const convertTime = (datetime: string) => {
+        const date = new Date(datetime);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+    };
+
+    return <div className="flex w-full flex-col gap-4 md:p-4 lg:p-8 rounded-xl shadow-sm md:bg-white">
+        <h2 className='uppercase font-bold text-lg md:text-2xl text-[#333]'>{title}</h2>
+        {rides.length? (<ul className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-8'>
+                {rides.map((ride: RIDE_TYPE, index: number) => <li key={index}
+                                                                   className='bg-[#ffffff8f] border p-4 rounded-xl flex flex-col gap-3'>
+                    <div className="flex gap-3 items-center">
+                        <Avatar>
+                            <AvatarImage src={ride?.driver?.photo}/>
+                            <AvatarFallback>{`${ride?.driver?.first_name.at(0)}${ride?.driver?.last_name.at(0)}`}</AvatarFallback>
+                        </Avatar>
+                        <div
+                            className="uppercase font-semibold">{ride?.driver?.first_name} {ride?.driver?.last_name}</div>
+                    </div>
+                    <div className="">{ride?.from} - {ride?.to}</div>
+                    <div className="text-muted-foreground text-sm">At {convertTime(`${ride?.time}`)}</div>
+                    <Button asChild><Link href={`/riders/book-ride/${ride?._id}`}>Book</Link></Button>
+                </li>)}
+        </ul>): (<div className={'flex flex-col gap-4 justify-center items-center'}>
+            <Image src={NO_RIDES} alt={'No Rides'} className="block h-[200px] w-auto" />
+            <p className={'text-muted-foreground text-base'}>No rides found</p>
+        </div>)
+        }
+
+    </div>
+}
+
+export default Rides;
