7c70dc174ba9e0a200cff02cf3533ec0ecfea487
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-06-15 21:14:19 +0300

    created links in the topbar

---
+++ client/src/components/Links.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Link from "next/link";
+import { signOut } from 'next-auth/react';
+import {Button} from "@/components/ui/button";
+import {CarFront, MessageCircle} from "lucide-react";
+
+
+const Links =() =>{
+    return <ul className={'flex items-center gap-4'}>
+        <li title={'Trips'}><Link href={'/'} className={'h-[30px] w-[30px] hover:bg-[#f1f1f1] flex items-center justify-center rounded-full '}><CarFront size={22}/></Link></li>
+        <li title={'Chat'}><Link href={'/'} className={'h-[30px] w-[30px] hover:bg-[#f1f1f1] flex items-center justify-center rounded-full'}><MessageCircle size={18}/></Link></li>
+        <li title={'Logout'}><Button variant={'ghost'} onClick={signOut}>Logout</Button></li>
+    </ul>
+}
+
+export default Links;
